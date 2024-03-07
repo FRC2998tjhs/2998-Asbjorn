8b5eaf04febf4cec7ddee9d49f0aa610e3266409
Shelby Doolittle <shelby@shelbyd.com>
2024-03-07 11:01:19 -0500

    Add failing test for Transmission.

---
+++ src/test/java/robot/subsystems/transmission/TransmissionTest.java
@@ -0,0 +1,9 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TransmissionTest {
+    @Test
+    public void noInput_isEmpty() {
+        assertEquals(1, 2);
+    }
+}
